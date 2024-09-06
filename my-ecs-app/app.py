9872b002c77965f4aaf77dea48ff38d6770a7e59
SuryaRajud09 <121481386+SuryaRajud09@users.noreply.github.com>
2024-09-06 11:39:24 +0530

    Delete my-ecs-app directory

--- my-ecs-app/app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import aws_cdk as cdk
-from my_ecs_app.my_ecs_app_stack import MyNewCdkAppStack
-
-app = cdk.App()
-MyNewCdkAppStack(app, "MyNewCdkAppStack")
-
-app.synth()
-
